9f84671a6386e823c05bea2ca8d33021f565f530
Zeel Haria <hariazeel@gmail.com>
2022-03-16 20:36:33 +0530

    Merge branch 'main' of https://github.com/uvcreation/portfolio into main_zeel

--- js/modal.js
+++ js/modal.js
@@ -1,61 +1,61 @@
-const images = document.querySelectorAll(".images img");
-const modal = document.querySelector(".modal");
-const modalImg = document.querySelector(".modalImg");
-const modalTxt = document.querySelector(".modalTxt");
+const img = document.querySelectorAll(".img img");
+const modal1 = document.querySelector(".modal1");
+const modal1Img = document.querySelector(".modalImg");
+const modal1Txt = document.querySelector(".modalTxt");
 const close = document.querySelector(".close");
 const prevBtn = document.querySelector(".prevBtn");
 const nextBtn = document.querySelector(".nextBtn");
 
-images.forEach((image, index) => {
+img.forEach((image, index) => {
   
   image.addEventListener("click", () => {
     
     modalImg.src = image.src;
     modalTxt.innerHTML = image.alt;
-    modal.classList.add("appear");
+    modal1.classList.add("appear");
 
     let imageIndex = index;
     let next = imageIndex++;
     let prev = imageIndex--;
 
     window.addEventListener("keyup", (e) => {
-      /*if (next >= images.length) {
-              next = 0;
-            } else if (prev < 0) {
-              prev = images.length - 1;
-            }*/
+      // if (next >= img.length) {
+      //         next = 0;
+      //       } else if (prev < 0) {
+      //         prev = img.length - 1;
+      //       }
 
       if (e.keyCode === 37) {
-        modalImg.src = images[prev].src;
-        modalTxt.innerHTML = images[prev].alt;
+        modalImg.src = img[prev].src;
+        modalTxt.innerHTML = img[prev].alt;
         prev--;
         next = prev + 2;
       } else if (e.keyCode === 39) {
-        modalImg.src = images[next].src;
-        modalTxt.innerHTML = images[next].alt;
+        modalImg.src = img[next].src;
+        modalTxt.innerHTML = img[next].alt;
         next++;
         prev = next - 2;
       } else if (e.keyCode === 27) {
-        modal.classList.remove("appear");
+        modal1.classList.remove("appear");
       }
     });
 
     prevBtn.addEventListener("click", () => {
-      modalImg.src = images[prev].src;
-      modalTxt.innerHTML = images[prev].alt;
+      modalImg.src = img[prev].src;
+      modalTxt.innerHTML = img[prev].alt;
       prev--;
       next = prev + 2;
     });
 
     nextBtn.addEventListener("click", () => {
-      modalImg.src = images[next].src;
-      modalTxt.innerHTML = images[next].alt;
+      modalImg.src = img[next].src;
+      modalTxt.innerHTML = img[next].alt;
       next++;
       prev = next - 2;
     });
 
     close.addEventListener("click", () => {
-      modal.classList.remove("appear");
+      modal1.classList.remove("appear");
     });
   });
 });
@@ -75,5 +75,5 @@ images.forEach((image, index) => {
     });
 
     close.addEventListener("click", () => {
-      modal.classList.remove("appear");
-    });
+      modal1.classList.remove("appear");
+    }); 
